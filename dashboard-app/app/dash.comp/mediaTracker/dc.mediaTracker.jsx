72fada0a940a1ef66bf8f2d27eba22fd46876319
John Ramos <jramos5883@gmail.com>
2023-07-01 08:27:06 -0700

    Brainstorming for media app

--- dashboard-app/app/dash.comp/mediaTracker/dc.mediaTracker.jsx
+++ dashboard-app/app/dash.comp/mediaTracker/dc.mediaTracker.jsx
@@ -1,3 +1,5 @@
 export default function MediaTracker() {
   return <div>Media Tracker!</div>;
 }
+
+// Need to do some research on show/movie/media Apis, want to reserach what I can do for this app.
