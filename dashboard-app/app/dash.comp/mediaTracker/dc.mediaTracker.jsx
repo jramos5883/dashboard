afe01de521014ac0390cba05ca02ec989af5dd92
John Ramos <jramos5883@gmail.com>
2023-07-02 18:58:10 -0700

    still reasearching for media tracker!

--- dashboard-app/app/dash.comp/mediaTracker/dc.mediaTracker.jsx
+++ dashboard-app/app/dash.comp/mediaTracker/dc.mediaTracker.jsx
@@ -3,3 +3,4 @@ export default function MediaTracker() {
 }
 
 // Need to do some research on show/movie/media Apis, want to reserach what I can do for this app.
+// Want to find an APi that tracks movies and shows and can tell us how many episodes/season are out.
