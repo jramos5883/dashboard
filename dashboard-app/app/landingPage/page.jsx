4483b3080b7f1a893c341f8b14941342658e101e
John Ramos <jramos5883@gmail.com>
2023-07-04 12:24:09 -0700

    Touching up landing page to deploy, getting dashboard portfolio ready.

--- dashboard-app/app/landingPage/page.jsx
+++ dashboard-app/app/landingPage/page.jsx
@@ -1,6 +1,6 @@
 export default function LandingPage() {
   return (
-    <div className="homepage-container bg-red-300 h-screen">
+    <div className="homepage-container h-screen">
       <h1 className="text-5xl p-4">Landing Page!</h1>
       <p className="text-3xl p-4">Product Description</p>
       <div className="border-2 border-gray-700 flex justify-center justify-items-center items-center">
@@ -8,10 +8,12 @@ export default function LandingPage() {
       </div>
       <p className="text-3xl p-4">Dashboard features:</p>
       <ul>
-        <li className="text-2xl p-4 px-20">Infinite Scrolling</li>
-        <li className="text-2xl p-4 px-20">Google Calendar Api</li>
+        <li className="text-2xl p-4 px-20">Personal Embeded Google Calendar</li>
         <li className="text-2xl p-4 px-20">
-          Recent Dota 2 Tournament Match Results
+          ToDo, Budget, and Shopping List Apps
+        </li>
+        <li className="text-2xl p-4 px-20">
+          Recent Dota 2 Tournament Match Results Tracker
         </li>
       </ul>
     </div>
